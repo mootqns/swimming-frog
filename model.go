7a30589bab8957f478fcc321ad8b3f5c841d44e4
mootqns <mqn360@gmail.com>
2023-11-04 14:08:56 -0700

    frog emoticon

--- model.go
+++ model.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"fmt"
-
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/muesli/termenv"
-)
-
-var (
-	color   = termenv.EnvColorProfile().Color
-	keyword = termenv.Style{}.Foreground(color("204")).Background(color("235")).Styled
-	help    = termenv.Style{}.Foreground(color("241")).Styled
 )
 
 type model struct {
-	altscreen bool
-	quitting  bool
+	frog string
+}
+
+func initialModel() model {
+	return model{
+		frog: "🐸",
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -23,43 +19,10 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c", "esc":
-			m.quitting = true
-			return m, tea.Quit
-		case " ":
-			var cmd tea.Cmd
-			if m.altscreen {
-				cmd = tea.ExitAltScreen
-			} else {
-				cmd = tea.EnterAltScreen
-			}
-			m.altscreen = !m.altscreen
-			return m, cmd
-		}
-	}
 	return m, nil
 }
 
 func (m model) View() string {
-	if m.quitting {
-		return "Bye!\n"
-	}
-
-	const (
-		altscreenMode = " altscreen mode "
-		inlineMode    = " inline mode "
-	)
-
-	var mode string
-	if m.altscreen {
-		mode = altscreenMode
-	} else {
-		mode = inlineMode
-	}
-
-	return fmt.Sprintf("\n\n  You're in %s\n\n\n", keyword(mode)) +
-		help("  space: switch modes • q: exit\n")
+	s := m.frog
+	return s
 }
